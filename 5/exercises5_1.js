17016f811ea57e1deab6506389d8162035d862a1
Fahri Saputra <51977977+FahriSaputra@users.noreply.github.com>
2019-08-20 23:23:14 +0700

    Add files via upload

---
+++ 5/exercises5_1.js
@@ -0,0 +1,11 @@
+ var word = 'JavaScript ';
+ var second = 'is ';
+ var third = 'awesome ';
+ var fourth = 'and ';
+ var fifth = 'I ';
+ var sixth = 'love ';
+ var seventh = 'it!';
+
+ JavaScript is awesome and I love it!
+
+ console.log(word.concat(second+third+fourth+fifth+sixth+seventh))
